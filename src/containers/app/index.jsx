ace2f4b844ff4e8663f4e93cf74a6fdbd14422a9
eyobed101 <eyocjjazz101@gmail.com>
2023-07-29 20:27:55 +0300

    intial commit for reddit app clone

---
+++ src/containers/app/index.jsx
@@ -0,0 +1,17 @@
+import { React } from 'react'
+// import reactLogo from '../assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Reddit clone</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/utils/registerServiceWorker.jsx
@@ -0,0 +1,13 @@
+import { React } from 'react'
+
+
+function registerServiceWorker() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default registerServiceWorker
